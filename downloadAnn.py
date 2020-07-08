a02926f17015f4d7ba7cc2c58f71f622b7532be7
buhagee <56422269+buhagee@users.noreply.github.com>
2020-07-08 22:43:04 +1000

    Initial

---
+++ downloadAnn.py
@@ -0,0 +1,138 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import lxml
+import time
+import datetime
+import requests
+import re
+import sys
+import os 
+
+pdfPath = "announcement_files/pdfs/"
+
+start_time = datetime.datetime.now()
+
+today = str(datetime.date.today().strftime("%d-%m-%Y"))
+
+# Checks command line arguments
+if len(sys.argv) > 3: 
+	print("Usage: python3 downAnn <'yesterday' or 'today'> <dd-mm-yyyy>")
+	quit()
+elif len(sys.argv) > 1:
+	# Command line chooses whether to read today or yesterday's announcements
+	if sys.argv[1] == "yesterday":
+		# Yesterday announcements, changes date to previous trading day
+		annPage = "https://www.asx.com.au/asx/statistics/prevBusDayAnns.do"
+		today = datetime.date.today()-datetime.timedelta(days=1)
+		while today.weekday() > 4:
+			today -= datetime.timedelta(days=1)
+		today = str(today.strftime("%d-%m-%Y"))
+	elif sys.argv[1] == "today":
+		# Todays announcements
+		annPage = "https://www.asx.com.au/asx/statistics/todayAnns.do"
+	else: 
+		# Wrong usage
+		print("Usage: python3 downAnn <yesterday/today> <dd-mm-yyyy>")
+		quit()
+	if len(sys.argv) > 2:
+		# Manual date chosen
+		today = sys.argv[2]
+else:
+	# defaults to todays announcements
+	annPage = "https://www.asx.com.au/asx/statistics/todayAnns.do"
+
+
+options = webdriver.ChromeOptions() # Doesn't visually open up chrome screen
+options.add_argument("headless")
+
+# Opens ASX announcements page
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.get(annPage)
+content = driver.page_source
+soup = BeautifulSoup(content, 'lxml')
+
+# Picks table attribute
+table = soup.find("table")
+row = table.find_all('tr')
+
+startUrl = "https://www.asx.com.au"
+
+
+# Creates folder for todays announcements
+if not os.path.exists(pdfPath+today):
+    os.mkdir(pdfPath+today)
+
+count = 0
+
+for tr in row:
+
+	# To skip heading
+	check = tr.find('td')
+	if not check:
+		continue
+
+	# Check if price sensitive
+	check2 = tr.find('td', attrs={'class':'pricesens'})
+	if not check2:
+		continue
+		
+	# Company code
+	code = check.get_text()
+
+	# Time
+	time = tr.find('span', attrs={'class':'dates-time'}).get_text()[0:-3]
+	time = time[0:-3] + time[-2:]
+
+	# Grabs middle link
+	a = tr.find('a')
+	tempLink = "".join((startUrl, a.get('href'))) 
+
+	# Follows middle link
+	tempDriver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+	tempDriver.get(tempLink)
+	tempContent = tempDriver.page_source
+	tempSoup = BeautifulSoup(tempContent, 'lxml')
+
+	# Grabs announcement URL
+	tempURL = tempSoup.find('input', attrs={'name':'pdfURL'}).get('value')
+
+	tempDriver.close()
+
+	# PDF URL
+	url = "".join((startUrl, tempURL)) 
+
+	# Save path
+	path = pdfPath+today+'/'+code+'_'+time+'.pdf'
+
+	# Accounts for multiple company announcements in day 
+	i = 2
+	while 1:
+		newTime=time
+		if os.path.isfile(path):
+			newTime = time + '_' + str(i)
+			path = pdfPath+today+'/'+code+'_'+newTime+'.pdf'
+			i += 1
+		else:
+			time = newTime
+			break
+
+	# Downloads pdf to path
+	r = requests.get(url)
+	open(path, 'wb').write(r.content) 
+
+	count += 1
+	# Prints info to terminal
+	print("\nAnnouncement: "+code+'_'+time+". Download number: "+str(count)+". Time elapsed: "+str(datetime.datetime.now() - start_time))
+	break
+
+# Prints final info to terminal
+elapsed_time = datetime.datetime.now() - start_time
+print("\nDownloaded "+str(count)+" announcements to "+pdfPath+today+"'.\nTime elapsed: "+str(elapsed_time))
+
+tempDriver.quit()
+driver.quit()
+
+
---
+++ searchAnn.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+date="$1"
+pdfToTxt="true"
+
+pdfs="announcement_files/pdfs"
+texts="announcement_files/texts"
+reports="announcement_files/reports"
+
+# If no argument todays date is used
+if test $# != 1 >/dev/null
+then
+	date="$(date +'%d-%m-%Y')"
+fi
+
+# Checks arguments are correct and folders exist
+if [ ! -d "$pdfs/$date" ]
+then
+	if [ -d "$texts/$date" ]
+	then 
+		pdfToTxt="false"
+	else
+	echo "Directory '$pdfs/$date' DOES NOT exists."
+	echo "Usage: ./seachAnn.sh <dd-mm-yyyy>"
+	exit 1
+	fi
+fi
+
+# Converts all pdfs to txt, deletes pdfs and put txt into seperate folder
+# Doesn't overite txt file if it already exists
+if "$pdfToTxt" = "true"
+then
+	echo "Converting pdf files to txt..."
+
+	if [ ! -d "$texts/$date" ]
+	then
+		mkdir "$texts/$date"
+	fi
+
+	for file in "$pdfs/$date/"*
+	do 	
+		code=`echo "$file" | cut -d'/' -f4 | cut -c 1-3`
+		fileName=`echo "$file" | cut -d'/' -f4 | cut -d'.' -f1`
+		if [ ! -f "$texts/$date/$fileName.txt" ]
+		then
+			pdftotext "$file" "$texts/$date/$fileName.txt"
+		fi
+	done
+
+	# Removes directory containing pdf's
+	rm -r "$pdfs/$date"
+
+	echo "Conversion Completed and deleted pdf files.\nTxt files are located in the following dir: $texts/$date"
+
+fi
+
+reportNames="gold_drill_results"
+
+
+# Creates directory for todays reports
+if [ ! -d "announcements/reports/$date" ]
+then 
+	mkdir "announcements/reports/$date"
+fi
+
+# Deletes today's reports if they already exists
+for name in "$reportNames"
+do  
+	echo "$name"
+	if [ -f "$reports/$date/$name.txt" ]
+	then
+		rm "$reports/$date/$name.txt"
+	fi
+done
+
+# Egrep pattern, creates report
+# Might need to delete txt files after use
+for file in "$texts/$date/"*
+do
+	fileName=`echo "$file" | cut -d'/' -f4 | cut -d'.' -f1`
+	
+	# searches for gold with > 30.0 g/t gold/au (decimal points optional)
+	reportName="gold_drill_results"
+ 	goldDrillResults='(([1-9][1-9]|[ ][3-9])[0-9][ ]?g/t[ ]?(gold|au)|([1-9][1-9]|[ ][3-9])[0-9].[0-9]+[ ]?g/t[ ]?(gold|au))'
+	filter="$goldDrillResults"
+	# Sends gold_drill_results report to txt file
+
+	if egrep -q -i "$filter" "$file" 
+		then
+		echo "$fileName\n" >>"$reports/$date/$reportName.txt"
+		egrep -i "$goldDrillResults" "$file" >>"$reports/$date/$reportName.txt"
+		echo "\n" >>"$reports/$date/$reportName.txt"
+		count=`expr $count + 1`	
+	fi
+
+
+
+done
+
+count=0
+
+# Prints location of report
+for name in "$reportNames"
+do
+	if [ -f "announcements/reports/$date/$name.txt" ]
+	then
+		count=`expr $count + 1`
+	fi
+done
+
+echo "$count reports created and are located in the following dir:\n'$reports/$date'"
